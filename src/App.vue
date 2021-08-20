d8691fc168d578812f5728307ea66229aea539dc
rafie97 <rajosh80@gmail.com>
2021-08-20 18:59:07 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <HelloWorld msg="Hello Udundi" />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <img src="../assets/Coding-Challenge-Image.png" class="image-fit" />
+  <div data-aos="fade-in">{{ msg }}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import AOS from "aos";
+
+export default defineComponent({
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      scrolled: 0,
+    };
+  },
+  mounted() {
+    AOS.init();
+  },
+  methods: {
+    onScroll() {
+      this.scrolled += 1;
+    },
+  },
+});
+</script>
